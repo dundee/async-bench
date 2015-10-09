e97afc15da44eb3549f15efc4c039a3cfef221ba
Daniel Milde <daniel@milde.cz>
2015-10-09 17:48:16 +0200

    handle socket closed

--- tornado_tcp_hello.py
+++ tornado_tcp_hello.py
@@ -1,13 +1,17 @@
 from tornado.tcpserver import TCPServer
 from tornado.ioloop import IOLoop
 from tornado.gen import coroutine
+import tornado
 
 class Handler(TCPServer):
     @coroutine
     def handle_stream(self, stream, address):
-       yield stream.read_until(b"\r\n\r\n")
-       stream.write('Hello world!')
-       stream.close()
+       try:
+           yield stream.read_until(b"\r\n\r\n")
+           yield stream.write('Hello world!')
+           stream.close()
+       except tornado.iostream.StreamClosedError:
+            pass
 
 server = Handler()
 server.listen(9000)
